9805c8c447d91aec9247aa5e6bb4a3f5fb9a6ea4
Adam Murdoch <adam.murdoch@gradleware.com>
2011-11-22 19:45:43 +1100

    Added some command-line parsing.

--- src/main/java/net/rubygrapefruit/docs/launcher/Main.java
+++ src/main/java/net/rubygrapefruit/docs/launcher/Main.java
@@ -1,5 +1,8 @@
 package net.rubygrapefruit.docs.launcher;
 
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import net.rubygrapefruit.docs.docbook.DocbookParser;
 import net.rubygrapefruit.docs.markdown.MarkdownParser;
 import net.rubygrapefruit.docs.model.Document;
@@ -9,16 +12,34 @@ import net.rubygrapefruit.docs.renderer.PdfRenderer;
 import net.rubygrapefruit.docs.renderer.Renderer;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<File> inputs = new ArrayList<File>();
-        for (int i = 0; i < args.length - 1; i++) {
-            inputs.add(new File(args[i]));
+    public static void main(String[] args) throws IOException {
+        OptionParser optionParser = new OptionParser();
+        optionParser.accepts("html", "Generate HTML 4 output");
+        optionParser.accepts("pdf", "Generate PDF output");
+        optionParser.accepts("out", "The directory to generate output to").withRequiredArg().required();
+
+        OptionSet result = null;
+        try {
+            result = optionParser.parse(args);
+        } catch (OptionException e) {
+            System.err.println(e.getMessage());
+            optionParser.printHelpOn(System.err);
+            System.exit(1);
         }
-        File outputDir = new File(args[args.length - 1]);
+
+        List<File> inputs = new ArrayList<File>();
+        for (String s : result.nonOptionArguments()) {
+            inputs.add(new File(s));
+        }
+
+        boolean html = result.has("html");
+        boolean pdf = result.has("pdf");
+        File outputDir = new File(result.valueOf("out").toString());
 
         Renderer htmlRenderer = new HtmlRenderer();
         Renderer pdfRenderer = new PdfRenderer();
@@ -31,11 +52,15 @@ public class Main {
             }
             Document document = parser.parse(input);
 
-            File htmlOutput = new File(outputDir, input.getName() + ".html");
-            htmlRenderer.render(document, htmlOutput);
+            if (html) {
+                File htmlOutput = new File(outputDir, input.getName() + ".html");
+                htmlRenderer.render(document, htmlOutput);
+            }
 
-            File pdfOutput = new File(outputDir, input.getName() + ".pdf");
-            pdfRenderer.render(document, pdfOutput);
+            if (pdf) {
+                File pdfOutput = new File(outputDir, input.getName() + ".pdf");
+                pdfRenderer.render(document, pdfOutput);
+            }
         }
     }
 }
